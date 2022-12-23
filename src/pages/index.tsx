8d0848578def8f3ab33a3812e471c0bb0f502592
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-22 22:18:55 -0400

    function for when there is no user to return to the login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
+import {  Container } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Home: NextPage = () => {
-  return (
-    <h1>Hello</h1>
-  )
-}
 
+  const router = useRouter();
+  const { user, isLoading, token } = useLogin();
+
+  console.log('user: ', user);
+  console.log('token: ', token);
+
+  useEffect(() => {
+    if (!user && !token) {
+      router.push("/login")
+    }
+  }, [user, isLoading, router])
+  if (isLoading) {
+    return <div>Loading</div>
+  } else {
+    return (
+      <Container>
+        Bem vindo :)
+      </Container>
+    )
+  }
+}
 export default Home
